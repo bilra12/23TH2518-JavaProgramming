23761edb51319061048f9c093262f929194767cb
thanh <k14qtm21@gmail.com>
2026-08-01 21:42:20 +0700

    lab1
    lab1

---
+++ HelloWorld/src/tiile/edu/xinchao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package tiile.edu;
+
+/**
+ * 
+ */
+public class xinchao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("toi hoc lai mon lap trinh de tien bo hon");
+	}
+
+}
