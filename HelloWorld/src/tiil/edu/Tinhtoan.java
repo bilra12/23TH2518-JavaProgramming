b79c92c60aedc47261ae860ca9d3419d36e61f5b
thanh <k14qtm21@gmail.com>
2026-08-01 20:54:09 +0700

    lab2
    lab2

---
+++ HelloWorld/src/tiil/edu/Tinhtoan.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package tiil.edu;
+
+/**
+ * 
+ */
+public class Tinhtoan {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+        int b = 5;
+
+        System.out.println("Tong = " + (a + b));
+        System.out.println("Hieu = " + (a - b));
+        System.out.println("Tich = " + (a * b));
+        System.out.println("Thuong = " + (a / b));
+	}
+
+}
