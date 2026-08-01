cb3f2cdd8c459cf84bdf243ae12b935b3689a5fa
thanh <k14qtm21@gmail.com>
2026-08-01 21:29:10 +0700

    lab4
    lab4

---
+++ laptrinhhuongdoituong/src/laptrinhhuongdoituong/MayTinh.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package laptrinhhuongdoituong;
+
+/**
+ * 
+ */
+public class MayTinh {
+
+		  int a;
+		    int b;
+
+		    int tong() {
+		        return a + b;
+		    }
+
+		    int hieu() {
+		        return a - b;
+		    }
+
+		    int tich() {
+		        return a * b;
+		    }
+
+		    int thuong() {
+		        return a / b;
+	}
+
+}
