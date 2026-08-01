c16309c9b0209bab372771ba1305f0918fcfab01
thanh <k14qtm21@gmail.com>
2026-08-01 21:04:43 +0700

    lab2
    lab2

---
+++ BienSo/src/tiil/edu/tinhtong.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package tiil.edu;
+
+/**
+ * 
+ */
+public class tinhtong {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 200;
+        int b = 5;
+
+        System.out.println("Tong = " + (a + b));
+        System.out.println("Hieu = " + (a - b));
+        System.out.println("Tich = " + (a * b));
+        System.out.println("Thuong = " + (a / b));
+	}
+
+}
