a877416162ac9599442e4a7c46ecd51552d58e3f
thanh <k14qtm21@gmail.com>
2026-08-01 21:36:36 +0700

    lab5
    lab5

---
+++ Array/src/tille/du/array.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package tille.du;
+import java.util.ArrayList;
+
+/**
+ * 
+ */
+public class array {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   ArrayList<String> ds = new ArrayList<>();
+
+	        ds.add("An");
+	        ds.add("Binh");
+	        ds.add("Cuong");
+
+	        System.out.println(ds);
+	}
+
+}
